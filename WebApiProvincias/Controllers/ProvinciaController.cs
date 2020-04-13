01ddbb868bb1b568d84889db5ee698a31f62721d
Cristian Torres <cristianlp.torr@gmail.com>
2020-04-12 23:19:38 -0300

    modificaciones en el Log

--- WebApiProvincias/Controllers/ProvinciaController.cs
+++ WebApiProvincias/Controllers/ProvinciaController.cs
@@ -31,24 +31,34 @@ namespace WebApiProvincias.Controllers
         [HttpGet("{nombre}")]
         public IActionResult GetProvincia(string nombre)
         {
-            using (var client = new HttpClient())
+            try
             {
+                using (var client = new HttpClient())
+                {
 
-                var _url = $"https://apis.datos.gob.ar/georef/api/provincias?nombre={nombre}";
-                Uri _uri = new Uri(_url);
-                var responseTask =  client.GetStringAsync(_uri);
+                    var _url = $"https://apis.datos.gob.ar/georef/api/provincias?nombre={nombre}";
+                    Uri _uri = new Uri(_url);
+                    var responseTask = client.GetStringAsync(_uri);
+                    _logger.LogInformation($"Se realizo la busqueda de la provincia: { nombre }");
 
-                JObject _jsonResponse = JObject.Parse(responseTask.Result);
-                var _provinciasEncontradas = (JArray)_jsonResponse["provincias"];
-                if (_provinciasEncontradas.Count == 0){
-                    var _coordenadas = new { latitud = 0, longitud = 0 };
-                    return NotFound(new { provincia = nombre,coordenadas=_coordenadas }) ;
+                    JObject _jsonResponse = JObject.Parse(responseTask.Result);
+                    var _provinciasEncontradas = (JArray)_jsonResponse["provincias"];
+                    if (_provinciasEncontradas.Count == 0)
+                    {
+                        var _coordenadas = new { latitud = 0, longitud = 0 };
+                        return NotFound(new { provincia = nombre, coordenadas = _coordenadas });
+                    }
+
+                    var latitud = _jsonResponse["provincias"][0]["centroide"]["lat"];
+                    var longitud = (string)_jsonResponse["provincias"][0]["centroide"]["lon"];
+                    ProvinciaDTO _DTOresponse = new ProvinciaDTO(nombre, latitud.ToString(), longitud);
+                    return Ok(_DTOresponse);
                 }
-
-                var latitud = _jsonResponse["provincias"][0]["centroide"]["lat"];
-                var longitud = (string)_jsonResponse["provincias"][0]["centroide"]["lon"];
-                ProvinciaDTO _DTOresponse = new ProvinciaDTO(nombre,latitud.ToString(),longitud);
-                return Ok(_DTOresponse);
+            }
+            catch(Exception exception)
+            {
+                _logger.LogInformation($"Fallo en la busqueda de la provincia: { nombre } -- Problema: {exception.Message}");
+                throw new Exception($"Problema al intentar realizar la busqueda de Provincia: {nombre}");
             }
         }
     }
